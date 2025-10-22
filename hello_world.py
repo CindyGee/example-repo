ee619dfee3caa88f28f555ce2b22da70a30f17da
Cindy Geldenhuys <pleaseemailcindy@gmail.com>
2025-10-22 13:16:11 +0200

    Hello world program

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+# Request user's name
+# Store user's input to the variable name
+# Print name
+# Request user's age
+# Store user's input to the variable age
+# Print age
+# Print "Hello, World!"
+
+name = input("Enter your name: ")
+print (name)
+age = input("Enter your age: ")
+print (age)
+print("Hello, World!")
